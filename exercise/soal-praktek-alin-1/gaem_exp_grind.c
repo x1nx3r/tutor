05a21a6443d03921c86de9e7c5478986de0b87f2
Mega Nugraha <monmega110@gmail.com>
2022-09-29 02:28:16 +0700

    commit gaem_exp_grind final, plus limiter 10^19

--- exercise/soal-praktek-alin-1/gaem_exp_grind.c
+++ exercise/soal-praktek-alin-1/gaem_exp_grind.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
-int slm[2]={3, 6}, hlc[2]={4, 8}, aby[2]={5, 10};
-int N, excess_value, aby_multiplier, hlc_multiplier, slm_multiplier;
-int slm_atk, slm_cost, hlc_atk, hlc_cost, aby_atk, aby_cost, total_atk, total_cost;
+unsigned long long slm[2]={3, 6}, hlc[2]={4, 8}, aby[2]={5, 10};
+unsigned long long N, excess_value, aby_multiplier, hlc_multiplier, slm_multiplier;
+unsigned long long slm_atk, slm_cost, hlc_atk, hlc_cost, aby_atk, aby_cost, total_atk, total_cost;
 // gaem likes to kill powerful monsters
 
 int main(){
-    scanf("%d", &N);
+    scanf("%llu", &N);
+    if (N>pow(10,19) || N<1){
+        printf("Enter a value between 1 to 10^19");
+        return 0;
+    } else {
+
+    }
     excess_value=N%10;
     aby_multiplier=(N-excess_value)/10;
     if(excess_value==8){
@@ -16,7 +23,7 @@ int main(){
         slm_multiplier=2;
     } else if (excess_value==7){
         hlc_multiplier=1;
-    } else {
+    } else if (excess_value<7 && excess_value>0 && ceil(excess_value)==floor(excess_value)) {
         slm_multiplier=1;
     }
     slm_atk=slm[0]*slm_multiplier;
@@ -29,15 +36,15 @@ int main(){
     total_atk=slm_atk+hlc_atk+aby_atk;
     total_cost=slm_cost+hlc_cost+aby_cost;
 
-    printf("is the desired exp to be gained : %d\n",N);
-    printf("is total attack happened %d\n",total_atk);
-    printf("is total aby killed : %d\n",aby_multiplier);
-    printf("is total hlc killed : %d\n",hlc_multiplier);
-    printf("is total slm killed : %d\n",slm_multiplier);
-    printf("is total aby exp acquired : %d\n",aby_cost);
-    printf("is total hlc exp acquired : %d\n",hlc_cost);
-    printf("is total slm exp acquired : %d\n",slm_cost);
-    printf("is the acquired exp : %d\n",total_cost);
+    printf("is the desired exp to be gained : %llu\n",N);
+    printf("is total attack happened %llu\n",total_atk);
+    printf("is total aby killed : %llu\n",aby_multiplier);
+    printf("is total hlc killed : %llu\n",hlc_multiplier);
+    printf("is total slm killed : %llu\n",slm_multiplier);
+    printf("is total aby exp acquired : %llu\n",aby_cost);
+    printf("is total hlc exp acquired : %llu\n",hlc_cost);
+    printf("is total slm exp acquired : %llu\n",slm_cost);
+    printf("is the acquired exp : %llu\n",total_cost);
 
     return 0;
 }
