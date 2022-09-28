6501be21da3182af3c234fa98410375579425a2b
Mega Nugraha <monmega110@gmail.com>
2022-09-29 01:05:02 +0700

    revisi ditambah limiter

---
+++ c_tutor/graphics.h/graphic_intro.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+double N;
+double A;
+double W=2;
+int main(){
+    scanf("%d",&N);
+    double X=sqrt(N);
+    printf("squrt = %lf",N);
+    /*
+    A=sqrt(N);
+    if (A==W){
+        printf("Ya");
+    } else {
+        printf("Tidak");
+    }
+    */
+    return 0;
+}
--- exercise/soal-praktek-alin-1/power_2.c
+++ exercise/soal-praktek-alin-1/power_2.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
-int N;
+#include <math.h>
 
+unsigned long long N;
+double A;
+unsigned long long W=2;
+double B;
+double C;
 int main(){
     scanf("%d",&N);
-    if (N%2==0){
+    if (N<=1){
+        printf("enter a value of 2 <= N <= 18446744073709551615");
+        return 0;
+    } else if (N==2) {
+        printf("Ya");
+        return 0;
+    } else{
+    }
+    A=(sqrt(N));
+    B=modf(A, &C);
+    if (B==0.0){
         printf("Ya");
     } else {
         printf("Tidak");
     }
+    return 0;
 }
